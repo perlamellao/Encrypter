205f3957b8784bcc9293d7fc2b8f12b57995467d
Jaime Villa <jaimevilla@MacBook-Pro-de-Jaime.local>
2020-05-07 21:19:32 +0200

    Del test

--- test/test.py
+++
@@ -1,4 +0,0 @@
-import requests
-r = requests.post("http://p3rl4.ddns.net:5000", data={'foo': 'soy raspberry pi 1'})
-# And done.
-print(r.text) # displays the result body.
--- test/test1.py
+++
@@ -1,4 +0,0 @@
-import subprocess
-import sys
-with subprocess.Popen(["ssh", "%s" % "pi@192.168.1.108", "ls"], shell=False,stdout=subprocess.PIPE,stderr=subprocess.PIPE) as ssh:
-    result = ssh.stdout.readlines()
