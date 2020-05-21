6924369b82050a59ac87d48c7de38f17fec40043
Jaime Villa <jaimevilla@MacBook-Pro-de-Jaime.local>
2020-05-21 16:33:47 +0200

    fixed to work in new ports

--- static/files/running.py
+++ static/files/running.py
@@ -2,7 +2,7 @@ import xml.etree.ElementTree as ET
 from urllib.request import urlopen
 def check():
     act = 0
-    tree = ET.parse(urlopen('http://www.p3rl4.me/riel/pi.xml'))
+    tree = ET.parse(urlopen('http://www.p3rl4.me:81/riel/pi.xml'))
     for x in [1,2,3,4,5]:
         pidir='.//raspi'+str(x)
         on = tree.find(pidir).text
