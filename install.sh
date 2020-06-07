28f09fc647c5002a4610d3f500aeb0a8af6b6a22
Perlamellao <perla@pop-os.localdomain>
2020-06-07 19:10:22 +0200

    Added No resize pyfladesk

--- install.sh
+++ install.sh
@@ -3,3 +3,4 @@ apt update
 apt upgrade
 apt install appmenu-gtk3-module
 pip3 install -r requirements.txt
+sed -i '59i\    window.setFixedSize(width, height)' /usr/local/lib/python3.8/dist-packages/pyfladesk/__init__.py
